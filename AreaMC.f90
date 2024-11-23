0b5561a72479b4ef7d7834541c039fb7775a3465
Murilo Garcia <murilomiglugy@gmail.com>
2024-11-22 22:06:55 -0300

    Introdução Criada

---
+++ AreaMC.f90
@@ -0,0 +1,17 @@
+program AreaMC
+!-----------------------------INTRODUÇÃO-----------------------------------
+!   Author: Murilo Garcia
+!   Date: 22/11/2024
+!   email:murilo.garcia@ufv.br    
+
+    !O programa consiste em encontrar uma aproximação para pi utilizando
+    !a técnica de Monte Carlo via razão de áreas.
+    !Para isso utilizamos do fato de que a razão entre a área de uma
+    !circunferência inscrita em um quadrado de lado unitário é igual
+    !a 1/4 de pi. Nesse sentido o algoritmo de Monte Carlo nos dará
+    !quantos pontos existem dentro da circunferência (uma espécie de área
+    !infinitesimal) quando o número de pontos totais (do quadrado) é previamente
+    !estabelecido
+!----------------------------------------------------------------------------------
+    
+end program
