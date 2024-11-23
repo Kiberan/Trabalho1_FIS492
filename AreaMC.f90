a91ee563186b1898393714a2add1a39ba1842449
Murilo Garcia <murilomiglugy@gmail.com>
2024-11-22 22:40:27 -0300

    Alterações no Laço e Escrita do Resultado + Arquivo.exe

--- AreaMC.f90
+++ AreaMC.f90
@@ -21,11 +21,11 @@ program AreaMC
 
     real(kind=dp) :: x
     real(kind=dp) :: y  !Coordenadas (x,y) de um ponto do Quadrado
-    integer(kind=i8) :: ciclos  !Ciclos corresponderá a quantidade de pontos
+    integer(kind=i8) :: ciclos = 10000  !Ciclos corresponderá a quantidade de pontos
                                 !totais para o cálculo da aproximação
                                 !Será nossa variável auxiliar para o laço
                                 !de repetição
-    real(kind=dp) :: dentro     !Responsável pela contagem de pontos dentro
+    real(kind=dp) :: dentro = 0    !Responsável pela contagem de pontos dentro
                                 !da circunferência
     real(kind=dp) :: razao      !Talvez eu use essa variável para a lógica
     integer(kind=i8) :: k       !Variável auxiliar para o Laço de Repetição
@@ -33,9 +33,10 @@ program AreaMC
     do k=1, ciclos
         call random_number(x)
         call random_number(y)
-        write(*,*) ciclos-k
         if (x**2 + y**2 < 1) then
             dentro = dentro + 1
         end if
     end do
+    write(*,*) 4*  real(dentro,dp)/real(ciclos,dp)
+    read(*,*)k !Tecla apenas para fechar o programa no PowerShell
 end program
